baf534567e4bebcdd6a5d7e5f848e2ae0461c774
Dulio Denis <dulio.denis@yahoo.com>
2015-11-01 12:31:09 -0500

    Added calculation operations

--- Calcraft/Calcraft/ViewController.swift
+++ Calcraft/Calcraft/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "x"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    var buttonSound: AVAudioPlayer!
+    
+    var currentDisplay = ""
+    var leftValueString = ""
+    var rightValueString = ""
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
+    
+    // MARK: View Lifecyle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundURL)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    // MARK: Calculator Action Operations
+    
+    @IBAction func numberPressed(button: UIButton!) {
+        playSound()
+        
+        currentDisplay += "\(button.tag)"
+        outputLabel.text = currentDisplay
+    }
+    
+    
+    @IBAction func onDivideTapped(sender: AnyObject) {
+        processOperation(Operation.Divide)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onMultiplyTapped(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractTapped(sender: AnyObject) {
+        processOperation(Operation.Subtract)
     }
 
+    @IBAction func onAddTapped(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualsTapped(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            // check to see that the user didn't selected multiple operators in a row
+            if currentDisplay != "" {
+                rightValueString = currentDisplay
+                currentDisplay = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValueString)! * Double(rightValueString)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValueString)! / Double(rightValueString)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValueString)! + Double(rightValueString)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValueString)! - Double(rightValueString)!)"
+                }
+                
+                leftValueString = result
+                outputLabel.text = result
+            }
+            
+            currentOperation = op
+            
+        } else { // this is the first time an operation has been tapped
+            leftValueString = currentDisplay
+            currentDisplay = ""
+            currentOperation = op
+        }
+    }
+    
+    
+    func playSound() {
+        if buttonSound.playing {
+            buttonSound.stop()
+        }
+        buttonSound.play()
+        
+    }
 
 }
 
